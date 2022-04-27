f3bf16d1d44795a0a22095821438e880d2b180ee
Tommy Maldonado <tommymaldonado@dynamic-oit-vapornet100-10-9-215-3.princeton.edu>
2022-04-27 14:05:20 -0400

    debugging in linblad.py

--- linblad.py
+++ linblad.py
@@ -11,21 +11,23 @@ def factorial(i):
 			r *= i
 			i -= 1
 		return r
+@njit
+def ic(i, j, beta):
+	return np.exp(- np.abs(beta) ** 2) * beta ** (i + j) / (factorial(i)**(1/2)*factorial(j)**(1/2))
 
 @njit(parallel=True)
 def evolve(N, tf, nt, beta, kappa, K):	
 	dt = tf/nt
-	rho = np.zeros((nt,N,N), dtype = np.complex64)
+	rho = np.zeros((nt,N,N), dtype = np.complex128)
 	for i in prange(N):
 		for j in range(N):
 			# assuming beta is real (which it is)
-			denom = (factorial(i))**(1/2) * (factorial(j))**(1/2)
-			rho[0, i, j] = np.exp(- np.abs(beta) ** 2) * beta ** (i + j) / denom
+			#denom = (factorial(i))**(1/2) * (factorial(j))**(1/2)
+			rho[0, i, j] = np.exp(- np.abs(beta) ** 2) * beta ** (i + j) / (factorial(i)**(1/2)*factorial(j)**(1/2))
+	print(rho)
 	for t_i in range(nt-1):
 		for i in prange(N):
 			for j in range(N):
-				#if np.abs(rho[t_i, i, j]) > 10 ** 6:
-					#print(rho[t_i, i, j])
 				derivative = rho[t_i,i, j]* ((K/2)*(i*(i-1)-j*(j-1)) + kappa*(i+j)/2)
 				if i < N-1 and j < N-1:
 					derivative += rho[t_i,i+1,j+1]*kappa*(np.sqrt(i*j))
@@ -41,5 +43,7 @@ if __name__ == "__main__":
 	dt = (tf / nt) 
 	kappa = 2 * np.pi * 10 * 10 ** 3
 	K = 2 * np.pi * 325 * 10 ** 3
-	rho = evolve(N, tf, nt, beta, kappa, K)
-	print(rho)
+	for i in range(N):
+		for j in range(N):
+			print(ic(i, j, beta))
+	#rho = evolve(N, tf, nt, beta, kappa, K)
